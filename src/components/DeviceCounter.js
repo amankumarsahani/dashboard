23f41ea700c741f755eaaaa45ab8aeb64fb07b80
Ehsan ul haq Khawja <uehsan3@icloud.com>
2022-01-28 21:21:02 +0530

    Added dig parameter to control formatValue

--- src/components/DeviceCounter.js
+++ src/components/DeviceCounter.js
@@ -1,7 +1,7 @@
 import AnimatedNumber from "animated-number-react";
 import React from "react";
 
-export default function App({ data }) {
-  const formatValue = (data) => data.toFixed(0);
+export default function App({ data, dig = 0 }) {
+  const formatValue = (data) => data.toFixed(dig);
   return <AnimatedNumber value={data} formatValue={formatValue} />;
 }
