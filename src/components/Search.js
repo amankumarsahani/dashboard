bde0dce6fe5f5ff49dc4e55a4b76dafca6885790
Ehsan ul haq Khawja <uehsan3@icloud.com>
2022-01-28 21:45:44 +0530

    Uses ReactChart now doesn't refresh if data fetched is same as previous

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,29 +1,18 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import AccChart from "./charts/AccChart";
-import TempChart from "./charts/TempChart";
+import ReactChart from "./charts/ReactChart";
 import "./Search.css";
-import {
-  makeAccData,
-  makeTempData,
-  makeAccDataFromQuery,
-  makeTempDataFromQuery,
-} from "../utils.js";
+import { makeAccDataFromQuery, makeTempDataFromQuery } from "../utils.js";
 
-export default function Search({ url, refresh, theme }) {
+export default function Search({ url, refresh, theme, vw }) {
   const [searchField, setSearchField] = useState("");
   const [id, setId] = useState("c3:83:0c:de:ae:07");
   const [searchLim, setSearchLim] = useState(10);
   const [lim, setLim] = useState(10);
   const [accOrTempData, setAccOrTempData] = useState("");
   const [sensor, setSensor] = useState("");
-
-  const type = {
-    dataX: "bar",
-    dataY: "bar",
-    dataZ: "bar",
-    abs: "bar",
-  };
+  const [pr, setPr] = useState(0);
 
   const handleSearchChange = (e) => {
     setSearchField(e.target.value);
@@ -37,11 +26,44 @@ export default function Search({ url, refresh, theme }) {
     setLim(searchLim);
   };
 
+  const handleNewResponse = (response) => {
+    if (sensor === "acc") {
+      if (response.data.accOrTemp[0].Sensor === "Accelerometer") {
+        let atd = makeAccDataFromQuery(response.data.accOrTemp, "time_stmp");
+        if (
+          !arrayEquals(atd.dataLabels, accOrTempData.dataLabels) ||
+          !arrayEquals(atd.abs, accOrTempData.abs) ||
+          !arrayEquals(atd.dataY, accOrTempData.dataY) ||
+          !arrayEquals(atd.dataZ, accOrTempData.dataZ)
+        ) {
+          setAccOrTempData(atd);
+        }
+      } else {
+        let atd = makeTempDataFromQuery(response.data.accOrTemp, "time_stmp");
+        setSensor("temp");
+        setAccOrTempData(atd);
+      }
+    } else {
+      if (response.data.accOrTemp[0].Sensor === "Temperature") {
+        let atd = makeTempDataFromQuery(response.data.accOrTemp, "time_stmp");
+        if (
+          !arrayEquals(atd.dataLabels, accOrTempData.dataLabels) ||
+          !arrayEquals(atd.temp, accOrTempData.temp)
+        ) {
+          setAccOrTempData(atd);
+        }
+      } else {
+        let atd = makeAccDataFromQuery(response.data.accOrTemp, "time_stmp");
+        setSensor("acc");
+        setAccOrTempData(atd);
+      }
+    }
+  };
+
   useEffect(() => {
-    axios
-      .get(url + "?id=" + id + "&lim=" + lim)
-      .then((response) => {
-        if (response !== []) {
+    axios.get(url + "?id=" + id + "&lim=" + lim).then((response) => {
+      if (response !== []) {
+        if (!accOrTempData) {
           if (response.data.accOrTemp[0].Sensor === "Accelerometer") {
             setSensor("acc");
             setAccOrTempData(
@@ -53,10 +75,21 @@ export default function Search({ url, refresh, theme }) {
               makeTempDataFromQuery(response.data.accOrTemp, "time_stmp")
             );
           }
-        }
-      })
-      .catch((err) => console.error(err));
+        } else handleNewResponse(response);
+      }
+    });
   }, [id, url, refresh, lim]);
+
+  document.getElementById("lim") &&
+    (document.getElementById("lim").oninput = function () {
+      var value = ((this.value - this.min) / (this.max - this.min)) * 100;
+      this.style.background =
+        "linear-gradient(to right, #82CFD0 0%, #82CFD0 " +
+        value +
+        "%, #fff " +
+        value +
+        "%, white 100%)";
+    });
 
   return (
     <div id="SearchContainer">
@@ -86,35 +119,85 @@ export default function Search({ url, refresh, theme }) {
           />
         </form>
       </div>
-      {sensor === "acc" && accOrTempData.abs && accOrTempData.abs.length !== 0 && (
-        <div className="searchResult">
-          <div className="searchData">
-            {parseFloat(
-              accOrTempData.abs[accOrTempData.abs.length - 1]
+      <div className="searchResult">
+        <div className="searchData">
+          {accOrTempData &&
+            accOrTempData[sensor === "acc" ? "abs" : "temp"] &&
+            parseFloat(
+              accOrTempData[sensor === "acc" ? "abs" : "temp"][
+                accOrTempData[sensor === "acc" ? "abs" : "temp"].length - 1
+              ]
             ).toFixed(2)}
-            &nbsp;m/s
-            {"\u00b2"}
-          </div>
-          <div className="searchChart">
-            <AccChart data={accOrTempData} type={type} theme={theme}></AccChart>
-          </div>
+          &nbsp; {sensor === "acc" ? " m/s\u00b2" : " ºC"}
         </div>
-      )}
-      {sensor === "temp" &&
-        accOrTempData.temp &&
-        accOrTempData.temp.length !== 0 && (
-          <div className="searchResult">
-            <div className="searchData">
-              {parseFloat(
-                accOrTempData.temp[accOrTempData.temp.length - 1]
-              ).toFixed(2)}
-              &nbsp; ºC
+        {sensor === "acc" &&
+          accOrTempData.abs &&
+          accOrTempData.abs.length !== 0 && (
+            <div
+              className="searchChart"
+              onClick={() => {
+                setPr(!pr);
+              }}
+            >
+              <ReactChart
+                title={"Accelerometer"}
+                xLabel={accOrTempData.dataLabels}
+                yLabelArr={["X-axis", "Y-axis", "Z-axis", "Magnitude"]}
+                yAxisIDArr={["y", "y", "y", "y"]}
+                yDataArr={[
+                  accOrTempData.dataX,
+                  accOrTempData.dataY,
+                  accOrTempData.dataZ,
+                  accOrTempData.abs,
+                ]}
+                typeArr={
+                  lim > 1
+                    ? ["line", "line", "line", "line"]
+                    : ["bar", "bar", "bar", "bar"]
+                }
+                colorArr={["#ff006f", "#f5a7a0", "#0fc26c", "#0062ff"]}
+                fillArr={[false, false, false, true]}
+                fontSize={vw}
+                gridColor={theme ? "#ffffff11" : "#00000022"}
+                pr={pr}
+              />
             </div>
+          )}
+        {sensor === "temp" &&
+          accOrTempData.temp &&
+          accOrTempData.temp.length !== 0 && (
             <div className="searchChart">
-              <TempChart data={accOrTempData}></TempChart>
+              <ReactChart
+                title={"Temperature"}
+                xLabel={accOrTempData.dataLabels}
+                yLabelArr={["Temperature ºC         "]}
+                yAxisIDArr={["y"]}
+                yDataArr={[accOrTempData.temp]}
+                typeArr={["bar"]}
+                colorArr={["#ff006f"]}
+                fillArr={[true]}
+                fontSize={vw}
+                gridColor={theme ? "#ffffff11" : "#00000022"}
+                pr={pr[0]}
+              />
             </div>
-          </div>
-        )}
+          )}
+      </div>
     </div>
+  );
+}
+
+function arrayEquals(a, b) {
+  console.log(
+    Array.isArray(a),
+    Array.isArray(b),
+    a.length === b.length,
+    a.every((val, index) => val === b[index])
+  );
+  return (
+    Array.isArray(a) &&
+    Array.isArray(b) &&
+    a.length === b.length &&
+    a.every((val, index) => val === b[index])
   );
 }
