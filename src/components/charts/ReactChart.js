a7770229f8086d1260bbec2d2dc391e64c548931
Ehsan ul haq Khawja <uehsan3@icloud.com>
2022-02-01 00:01:31 +0530

    Added multiple params, ref forwarding, fonts

--- src/components/charts/ReactChart.js
+++ src/components/charts/ReactChart.js
@@ -1,9 +1,11 @@
+import { saveAs } from "file-saver";
 import { Chart, registerables } from "chart.js";
 import { Line } from "react-chartjs-2";
-import { genRan, hexWithAlpha } from "../../utils.js";
+import { hexWithAlpha } from "../../utils.js";
+import { useState } from "react";
 
 Chart.register(...registerables);
-
+// Chart.global.defaults.font.family = "Exo 2, sans-serif";
 export default function ReactChart({
   title,
   xLabel,
@@ -12,48 +14,54 @@ export default function ReactChart({
   yDataArr,
   typeArr,
   colorArr,
+  legendColor,
   fillArr,
   fontSize = 14,
+  gridArr = [1, 1],
   gridColor = "#ffffff11",
-  pr = 0,
   giveIndex = 0,
+  offset = false,
+  forwardedRef,
 }) {
-  if (colorArr === []) {
-    yDataArr.forEach((_, i) => {
-      colorArr[i] = `hsl(
-        ${genRan(0, 360)},
-        100%,100%
-      )`;
-    });
-  }
+  const [pra, setPra] = useState(1);
   let scales = {
     y: {
       position: "left",
       beginAtZero: true,
       grid: {
-        display: true,
+        display: gridArr[1],
         color: gridColor,
       },
       ticks: {
-        font: { size: fontSize },
+        font: { size: fontSize * 0.9 },
       },
     },
     x: {
-      beginAtZero: true,
+      offset: offset,
       ticks: {
         maxRotation: 0,
         autoskip: true,
         autoSkipPadding: giveIndex ? 5 : 20,
-        font: { size: fontSize },
+        // major: {
+        //   enabled: true,
+        // },
+        font: { size: fontSize * 0.9 },
+        // font: function (context) {
+        //   if (context.tick && context.tick.major) {
+        //     return {
+        //       weight: "bold",
+        //     };
+        //   }
+        // },
       },
       grid: {
-        display: true,
+        display: gridArr[0],
         color: gridColor,
       },
     },
   };
 
-  const datasets = yDataArr.map((yData, i) => {
+  let datasets = yDataArr.map((yData, i) => {
     if (yAxisIDArr[i] === "y1") {
       scales.y1 = {
         position: "right",
@@ -80,19 +88,23 @@ export default function ReactChart({
   });
 
   let options = {
+    // defaultFontFamily: (Chart.defaults.global.defaultFontFamily =
+    //   "Exo 2, sans-serif"),
     responsive: true,
     maintainAspectRatio: false,
     scales: scales,
     plugins: {
       title: {
-        display: false,
+        display: title !== "",
         text: title,
         position: "top",
         align: "start",
-        color: "#00c3ff",
+        padding: 0,
+        color: legendColor,
         font: {
-          size: fontSize * 1.5,
-          weight: "200",
+          family: "Dongle",
+          size: fontSize * 3,
+          weight: "300",
         },
       },
       legend: {
@@ -100,16 +112,40 @@ export default function ReactChart({
         align: "end",
         padding: 1,
         labels: {
-          color: "#00c3ff",
+          color: legendColor,
           display: true,
           font: {
-            size: fontSize * 1.2,
+            size: fontSize * 0.9,
           },
         },
+        // //for reverse of default legend click
+        // onClick: function (e, legendItem) {
+        //   var index = legendItem.datasetIndex;
+        //   var ci = this.chart;
+        //   var alreadyHidden =
+        //     ci.getDatasetMeta(index).hidden === null
+        //       ? false
+        //       : ci.getDatasetMeta(index).hidden;
+
+        //   ci.data.datasets.forEach(function (e, i) {
+        //     var meta = ci.getDatasetMeta(i);
+
+        //     if (i !== index) {
+        //       if (!alreadyHidden) {
+        //         meta.hidden = meta.hidden === null ? !meta.hidden : null;
+        //       } else if (meta.hidden === null) {
+        //         meta.hidden = true;
+        //       }
+        //     } else if (i === index) {
+        //       meta.hidden = null;
+        //     }
+        //   });
+        //   ci.update();
+        // },
       },
       tooltip: {
         enabled: true,
-        // usePointStyle: true,
+        usePointStyle: true,
         callbacks: {
           // To change title in tooltip
           title: (data) => {
@@ -124,7 +160,7 @@ export default function ReactChart({
     },
     elements: {
       point: {
-        radius: pr * 3,
+        radius: pra * 3,
         hoverRadius: 10,
       },
     },
@@ -132,11 +168,13 @@ export default function ReactChart({
 
   return (
     <Line
+      ref={forwardedRef}
       data={{
         labels: giveIndex ? xLabel.map((_, i) => i + 1) : xLabel,
         datasets: datasets,
       }}
       options={options}
+      onClick={() => setPra(!pra)}
     />
   );
 }
